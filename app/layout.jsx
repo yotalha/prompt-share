50554b48fd7e843dce469e45c6328dcd26eb92ec
Talha Arshad <talha.arshad@systemsltd.com>
2024-04-16 18:00:05 +0500

    toggle dropdown for mobile view

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,26 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metaData = {
-    title: "Promptopia",
-    description: "Discover & share AI Prompts"
-}
+  title: "Promptopia",
+  description: "Discover & share AI Prompts",
+};
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-            <main className='app'>
-                {children}
-            </main>
-        </body>
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
